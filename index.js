899141a307ed3af8d49dcd28708839838e202da1
Luis García <luisgarcia@Luiss-MBP.lan>
2018-04-04 22:38:32 -0500

    add: engines

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => {
   res.send({ hi: 'there' })
 })
 
-app.listen(5000, () => {
-  console.log('Listening on PORT: 5000')
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Listening on PORT: ${PORT}`)
 })
